9698358ea1b0b24068f8690684ffd2629b5118fb
Marat Kasimov <kasimov.mv@mail.ru>
2019-07-25 02:35:29 +0400

    добавлен class Message

---
+++ app/src/main/java/com/example/weather_voise_assistant/Message.java
@@ -0,0 +1,16 @@
+package com.example.weather_voise_assistant;
+
+import java.util.Date;
+
+public class Message {
+    public String text;
+    public Date date;
+    public Boolean is_sent;
+
+    public Message(String text, Boolean is_sent) {
+        this.text = text;
+        this.date = new Date();
+        this.is_sent = is_sent;
+    }
+
+}

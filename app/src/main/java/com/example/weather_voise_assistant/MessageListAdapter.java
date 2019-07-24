9e550b5b5388da24938dee3a022a53dd9274b065
Marat Kasimov <kasimov.mv@mail.ru>
2019-07-25 03:09:45 +0400

    создание RecyclerView

--- app/src/main/java/com/example/weather_voise_assistant/MessageListAdapter.java
+++ app/src/main/java/com/example/weather_voise_assistant/MessageListAdapter.java
@@ -1,5 +1,37 @@
 package com.example.weather_voise_assistant;
 
-public class MessageListAdapter {
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class MessageListAdapter extends RecyclerView.Adapter {
+
+    private static final int ASSISTANT_TYPE = 0;
+    private static final int USER_TYPE = 1;
+
+    public List<Message> messageList = new ArrayList<>();
+
+    public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int index){
+        Message message = messageList.get(index);
+        ((MessageViewHolder)viewHolder).bind(message);
+    }
+
+    public RecyclerView.ViewHolder onCreateViewHolder (ViewGroup viewGroup, int view_type){
+        View view;
+        LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+        if (view_type == USER_TYPE){
+            view = inflater.inflate(R.layout.user_message, viewGroup, false);
+        }else {
+            view = inflater.inflate(R.layout.assistant_message, viewGroup, false);
+        }
+        return new MessageViewHolder(view);
+    }
+
+    public int getItemCount(){
+        return messageList.size();
+    }
 }
